4633588d36f18217c4dbb8430163335917f2521f
Kelby Ludwig <kelbyludwig@gmail.com>
2015-10-22 21:19:03 -0500

    Upgraded to IP-based connection

--- trudy.go
+++ trudy.go
@@ -1,41 +1,31 @@
 package main
 
 import (
-    "bufio"
     "net"
     "log"
     "encoding/hex"
 )
 
-var connection_counter uint
-
 func main(){
-    tcp_recvr, err := net.Listen("tcp", ":6666")
+    ip,_ := net.ResolveIPAddr("ip4", "0.0.0.0")
+    ip_conn, err := net.ListenIP("ip4:icmp", ip)
     if err != nil {
-        log.Fatal("Failed to setup TCP listener!")
+        log.Println("[FATAL] Failed to setup listener. Did you run this as root? (You should!)")
+        panic(err)
     }
     for {
-        tcp_conn, err := tcp_recvr.Accept()
+        packet_buffer := make([]byte, 65535)
+        n,dest_addr,err := ip_conn.ReadFrom(packet_buffer)
         if err != nil {
-            log.Println("[ERR] Error accepting a connection. Moving along.")
+            log.Println("[ERR] Error reading from connection. Moving along.")
             continue
         }
-        connection_counter++
-        log.Printf("[INFO] Connection %v accepted!\n", connection_counter)
-        go tcpHandle(tcp_conn, connection_counter)
+        log.Printf("[DEBUG] %v\n", dest_addr)
+        log.Printf("[INFO] Packet arrived!\n")
+        go packetHandle(packet_buffer[:n])
     }
 }
 
-func tcpHandle(conn net.Conn, ctr uint) {
-    defer conn.Close()
-    reader := bufio.NewReader(conn)
-    buffer := make([]byte, 1024)
-    for {
-        n,err := reader.Read(buffer)
-        if err != nil {
-            log.Printf("[INFO] Terminating connection %v!\n", ctr)
-            break
-        }
-        log.Printf("[DEBUG]: Input hexdump from connection %v\n%v", ctr, hex.Dump(buffer[:n]))
-    }
+func packetHandle(packet []byte) {
+    log.Printf("[DEBUG] Packet bytes\n%v", hex.Dump(packet))
 }
