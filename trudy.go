e41f11bf28726549edc82883b15c4bc94c9e8d02
Kelby Ludwig <kelbyludwig@gmail.com>
2015-10-26 21:37:50 -0500

    Basic idea is outlined. Need to get hardware for testing.

--- trudy.go
+++ trudy.go
@@ -7,14 +7,15 @@ import (
     "syscall"
 )
 
-
 const SO_ORIGINAL_DST = 80
 var connection_count uint
 
 type TCPPipe struct {
     id uint
-    original_destination_ip [16]byte
-    connection net.TCPConn
+    //TODO: destination_ip is a temporary struct. Using connections is all that is necessary.
+    destination_ip [16]byte
+    destination net.TCPConn
+    source net.TCPConn
 }
 
 func (t *TCPPipe) Id() uint {
@@ -22,11 +23,24 @@ func (t *TCPPipe) Id() uint {
 }
 
 func (t *TCPPipe) Close() {
-    t.connection.Close()
+    t.source.Close()
+    t.destination.Close()
 }
 
-func (t *TCPPipe) Read(buffer []byte) (n int, err error) {
-    return t.connection.Read(buffer)
+func (t *TCPPipe) ReadSource(buffer []byte) (n int, err error) {
+    return t.source.Read(buffer)
+}
+
+func (t *TCPPipe) WriteSource(buffer []byte) (n int, err error) {
+    return t.source.Write(buffer)
+}
+
+func (t *TCPPipe) ReadDestination(buffer []byte) (n int, err error) {
+    return t.destination.Read(buffer)
+}
+
+func (t *TCPPipe) WriteDestination(buffer []byte) (n int, err error) {
+    return t.destination.Write(buffer)
 }
 
 func newTCPPipe(id uint, conn net.TCPConn) TCPPipe {
@@ -34,33 +48,34 @@ func newTCPPipe(id uint, conn net.TCPConn) TCPPipe {
     if err != nil {
         log.Printf("[ERR] Failed to read connection file descriptor")
     }
-    fd := f.Fd()
-    log.Printf("[DEBUG] FD: %v\n", fd)
     //TODO: Investigate this more. This seems arbitrary. If a linux machine: syscall.SOL_IP
-    original,err := syscall.GetsockoptIPv6Mreq(int(fd), syscall.IPPROTO_IP, SO_ORIGINAL_DST)
-    log.Printf("[DEBUG] ORIGINAL %v\n", original.Multiaddr)
-    tcppipe := TCPPipe{id : id, connection: conn, original_destination_ip: original.Multiaddr}
+    original_destination,err := syscall.GetsockoptIPv6Mreq(int(f.Fd()), syscall.IPPROTO_IP, SO_ORIGINAL_DST)
     if err != nil {
-        log.Println("[ERR] Getting sockoption failed.")
-        log.Println(err)
+        log.Println("[ERR] Getsockopt failed. Error below:")
+        log.Printf("\t%v\n",err)
     }
+
+    //TODO: Construct the destination end of the pipe here too.
+    tcppipe := TCPPipe{id : id, source: conn, destination_ip: original_destination.Multiaddr}
+
     return tcppipe
 }
 
 func main(){
-    addr, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:6666")
-    listener,err := net.ListenTCP("tcp", addr)
+    tcpAddr, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:6666")
+    tcpListener,err := net.ListenTCP("tcp", tcpAddr)
 
-    defer listener.Close()
+    defer tcpListener.Close()
 
     if err != nil {
-        log.Println("[FATAl] Failed to setup listener. Did you run this as root? (You should!)")
+        log.Println("[FATAL] Failed to setup listeners. Did you run this as root? (You should!)")
         panic(err)
     }
 
     log.Println("[INFO] Trudy lives!")
+
     for {
-        conn, err := listener.AcceptTCP()
+        conn, err := tcpListener.AcceptTCP()
         if err != nil {
             log.Println("[ERR] Error reading from connection. Moving along.")
             continue
@@ -76,11 +91,23 @@ func tcpConnectionHandler(tcppipe TCPPipe) {
     defer log.Printf("[INFO] Connection %v closed!\n", tcppipe.Id())
     defer tcppipe.Close()
     buffer := make([]byte, 65535)
+    //TODO: A connection should run data through at most 2 functions. A filter (for packets that don't meet some criteria), and a modifier.
     for {
-        n, err := tcppipe.Read(buffer)
+        n, err := tcppipe.ReadSource(buffer)
         if err != nil {
             break
         }
+        if filter(buffer) {
+            buffer = mangle(buffer)
+        }
         log.Printf("[DEBUG] Packet bytes\n%v", hex.Dump(buffer[:n]))
     }
+}
+
+func filter(input []byte) bool {
+    return true
+}
+
+func mangle(input []byte) []byte {
+    return input
 }
