a9e1d16901e1a2a27f1d704b6734cc90c70dc037
Kelby Ludwig <kelbyludwig@gmail.com>
2015-10-28 20:23:59 -0500

    Two way pipe done. This needs a timeout though.

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+    "net"
+    "log"
+    "github.com/kelbyludwig/trudy/pipe"
+)
+
+var connection_count uint
+
+func main(){
+    tcpAddr, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:6666")
+    tcpListener,err := net.ListenTCP("tcp", tcpAddr)
+
+    defer tcpListener.Close()
+
+    if err != nil {
+        log.Println("[FATAL] Failed to setup listeners. Did you run this as root? (You should!)")
+        panic(err)
+    }
+
+    log.Println("[INFO] Trudy lives!")
+
+    for {
+
+        conn, err := tcpListener.AcceptTCP()
+        if err != nil {
+            log.Println("[ERR] Error reading from connection. Moving along.")
+            continue
+        }
+        tcppipe,err := pipe.NewTCPPipe(connection_count, *conn)
+        if err != nil {
+            log.Println("[ERR] Error creating new TCPPipe.")
+            continue
+        }
+        log.Printf("[INFO] TCP Connection %v accepted!\n", connection_count)
+        go clientHandler(tcppipe)
+        go serverHandler(tcppipe)
+        connection_count++
+    }
+}
+
+func clientHandler(tcppipe pipe.TCPPipe) {
+    defer tcppipe.Close()
+
+    buffer := make([]byte, 65535)
+
+    //TODO: Timeouts!
+    for {
+        bytesReadFromSource,err := tcppipe.ReadSource(buffer)
+        if err != nil {
+            log.Println("[ERR] Unable to read data from source.")
+            continue
+        }
+
+        //if filter(buffer[:bytesReadFromSource]) {
+        //    buffer = mangle(buffer[:bytesReadFromSource])
+        //}
+
+        _, err = tcppipe.WriteDestination(buffer[:bytesReadFromSource])
+        if err != nil {
+            log.Println("[ERR] Unable to send data to destination.")
+            continue
+        }
+    }
+}
+
+func serverHandler(tcppipe pipe.TCPPipe) {
+    defer tcppipe.Close()
+
+    buffer := make([]byte, 65535)
+
+    //TODO: Timeouts!
+    for {
+        bytesReadFromDestination,err := tcppipe.ReadDestination(buffer) 
+        if err != nil {
+            log.Println("[ERR] Unable to read from destination.")
+            continue
+        }
+        _,err = tcppipe.WriteSource(buffer[:bytesReadFromDestination])
+        if err != nil {
+            log.Println("[ERR] Unable to write to source.")
+            continue
+        }
+    }
+}
+
+func pass(input []byte) bool {
+    return true
+}
+
+func mangle(input []byte) []byte {
+    return input
+}
+
+func drop(input []byte) bool {
+    return false
+}
---
+++ pipe/pipe.go
@@ -0,0 +1,81 @@
+package pipe
+
+import (
+    "net"
+    "log"
+    "syscall"
+    "strconv"
+)
+
+//Netfilter/iptables adds a tcp header to identify original destination. 
+//Since all traffic is routed through trudy, we need to retrieve the original 
+//intended destination (i.e. _not_ trudy)
+const SO_ORIGINAL_DST = 80
+
+type TCPPipe struct {
+    id uint
+    destination net.Conn
+    source net.TCPConn
+}
+
+func (t *TCPPipe) Id() uint {
+    return t.id
+}
+
+func (t *TCPPipe) Close() {
+    log.Printf("[INFO] ( %v ) Closing connection.")
+    t.source.Close()
+    t.destination.Close()
+}
+
+func (t *TCPPipe) ReadSource(buffer []byte) (n int, err error) {
+    return t.source.Read(buffer)
+}
+
+func (t *TCPPipe) WriteSource(buffer []byte) (n int, err error) {
+    return t.source.Write(buffer)
+}
+
+func (t *TCPPipe) ReadDestination(buffer []byte) (n int, err error) {
+    return t.destination.Read(buffer)
+}
+
+func (t *TCPPipe) WriteDestination(buffer []byte) (n int, err error) {
+    return t.destination.Write(buffer)
+}
+
+//Converts the Multiaddr bytestring returned by Getsockopt into a "host:port" connection string.
+func ByteToConnString(multiaddr [16]byte) string {
+    ip := multiaddr[4:8]
+    ip_string := net.IPv4(ip[0], ip[1], ip[2], ip[3]).String()
+    port := multiaddr[2:4]
+    port_uint := int64((uint32(port[0]) << 8) + uint32(port[1]))
+    port_string := strconv.FormatInt(port_uint,10)
+    return (ip_string + ":" + port_string)
+}
+
+func NewTCPPipe(id uint, sourceConn net.TCPConn) (pipe TCPPipe, err error) {
+    tcppipe := new(TCPPipe)
+    f, err := sourceConn.File()
+    if err != nil {
+        log.Println("[DEBUG] Failed to read connection file descriptor.")
+        sourceConn.Close()
+        return *tcppipe, err
+    }
+    //TODO: Investigate this more. This seems arbitrary. If a linux machine: syscall.SOL_IP
+    originalAddrBytes,err := syscall.GetsockoptIPv6Mreq(int(f.Fd()), syscall.IPPROTO_IP, SO_ORIGINAL_DST)
+    if err != nil {
+        log.Println("[DEBUG] Getsockopt failed.")
+        sourceConn.Close()
+        return *tcppipe, err
+    }
+    destConn,err := net.Dial("tcp", ByteToConnString(originalAddrBytes.Multiaddr))
+    if err != nil {
+        log.Printf("[ERR] Unable to connect to destination. Closing connection %v.\n", id)
+        sourceConn.Close()
+        return *tcppipe, err
+    }
+    tcppipe = &TCPPipe{id : id, source: sourceConn, destination: destConn}
+    return *tcppipe, nil
+}
+
