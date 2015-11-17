863acc806c7594441b7d32f2deab971fc823ee13
Kelby Ludwig <kelbyludwig@gmail.com>
2015-11-17 09:02:24 -0600

    WebSocket wip

--- ws/ws.go
+++ ws/ws.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 import "io/ioutil"
 import "io"
-//import "github.com/gorilla/websocket"
+import "github.com/gorilla/websocket"
 
 var editor string
 
@@ -21,4 +21,25 @@ func EditorServer(w http.ResponseWriter, req *http.Request) {
     io.WriteString(w, editor)
 }
 
+func WebSocketServer(w http.ResponseWriter, req *http.Request) {
+    var upgrader = websocket.Upgrader{
+        ReadBufferSize:  65535,
+        WriteBufferSize: 65535,
+    }
+    conn, err := ugrader.Upgrade(w, r, nil)
+    if err != nil {
+        panic(err)
+    }
+}
 
+//Sends packets to the webrowser if the packet wants to be intercepted.
+func InterceptWriter(conn *websocket.Conn) {
+}
+
+//Retrieves packets from the web browser and sends them back through Trudy.
+func InterceptReader(conn *websocket.Conn) {
+
+}
+
+//document.getElementById('m').value = "42 42 42 42 42 42"
+//document.getElementById('m').oninput()
