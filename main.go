152d2e49cf0abc4581265479b23030b0125351cb
Kelby Ludwig <kelbyludwig@gmail.com>
2015-11-21 21:22:03 -0600

    Added hex editor

--- main.go
+++ main.go
@@ -149,7 +149,7 @@ func websocketHandler() {
     }
     upgrader := websocket.Upgrader{ ReadBufferSize: 65535, WriteBufferSize: 65535 }
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        io.WriteString(w, "hi")
+        io.WriteString(w, editor)
     })
     http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
         websocketConn, err = upgrader.Upgrade(w, r, nil)
@@ -163,3 +163,90 @@ func websocketHandler() {
         panic(err)
     }
 }
+
+const editor string = `<!-- this wonderful page was found here: https://github.com/xem/hex -->
+<body onload='
+// Reset the textarea value
+m.value="00";
+
+// Init the top cell content
+for(i=0;i<16;i++)
+  t.innerHTML+=(0+i.toString(16)).slice(-2)+" ";
+'>
+
+<!-- TRUDY SPECIFIC CODE ADDED FOR THIS PROJECT -->
+<h1> ~ Trudy Intercept ~ </h1>
+<script>
+    //TODO: This will have to be updated. Need to pull the address of the VM from the DOM.
+    var url = window.location.href
+    var arr = url.split("/");
+    var ws_url = "ws://" + arr[2] + "/ws"
+    var socket = new WebSocket(ws_url)
+    socket.onmessage = function (event) {
+        document.getElementById('m').value = event.data
+        document.getElementById('m').oninput()
+    }
+</script>
+<!-- END TRUDY SPECIFIC CODE -->
+</body>
+<table border><td><pre><td id=t><tr><td id=l width=80>00000000<td><textarea spellcheck=false id=m oninput='
+// On input, store the length of clean hex before the textarea caret in b
+b=value
+.substr(0,selectionStart)
+.replace(/[^0-9A-F]/ig,"")
+.replace(/(..)/g,"$1 ")
+.length;
+
+// Clean the textarea value
+value=value
+.replace(/[^0-9A-F]/ig,"")
+.replace(/(..)/g,"$1 ")
+.replace(/ $/,"")
+.toUpperCase();
+
+// Set the height of the textarea according to its length
+style.height=(1.5+value.length/47)+"em";
+
+// Reset h
+h="";
+
+// Loop on textarea lines
+for(i=0;i<value.length/48;i++)
+  
+  // Add line number to h
+  h+=(1E7+(16*i).toString(16)).slice(-8)+" ";
+
+// Write h on the left column
+l.innerHTML=h;
+
+// Reset h
+h="";
+
+// Loop on the hex values
+for(i=0;i<value.length;i+=3)
+  
+  // Convert them in numbers
+  c=parseInt(value.substr(i,2),16),
+  
+  // Convert in chars (if the charCode is in [64-126] (maybe more later)) or ".".
+  h=63<c&&127>c?h+String.fromCharCode(c):h+".";
+  
+// Write h in the right column (with line breaks every 16 chars)
+r.innerHTML=h.replace(/(.{16})/g,"$1 ");
+
+// If the caret position is after a space or a line break, place it at the previous index so we can use backspace to erase hex code
+if(value[b]==" ")
+  b--;
+
+// Put the textarea caret at the right place
+setSelectionRange(b,b)'
+cols=48></textarea><td width=160 id=r>.</td>
+</table>
+<style>
+*{margin:0;padding:0;vertical-align:top;font:1em/1em courier}
+#m{height:1.5em;resize:none;overflow:hidden}
+#t{padding:0 2px}
+#w{position:absolute;opacity:.001}
+</style>
+<button onclick="alert('sending does not work')">send</button>
+`
