de6a3c921d0d6e87d473d773a9e6bedaaddf8a03
Kelby Ludwig <kelbyludwig@gmail.com>
2015-11-26 15:52:08 -0600

    GoLinted module

--- module/module.go
+++ module/module.go
@@ -5,39 +5,40 @@ import (
 	"net"
 )
 
-//A wrapper that provides metadata that may be useful when mangling bytes on the network.
+//Data is a thin wrapper that provides metadata that may be useful when mangling bytes on the network.
 type Data struct {
 	FromClient bool
 	Bytes      []byte
 	DestAddr   net.Addr
 }
 
-//If this returns true, the data will be sent to the Mangle function.
+//DoMangle will return true, if Data needs to be sent to the Mangle function.
 func (input Data) DoMangle() bool {
 	return true
 }
 
-//This function should modify/replace the Bytes values within the Data struct.
+//Mangle can modify/replace the Bytes values within the Data struct. This can be empty if no
+//programmatic mangling needs to be done.
 func (input *Data) Mangle() {
 
 }
 
-//If this returns true, the data will not be sent to the other end of the pipe.
+//Drop will return true, if the Data needs to be dropped between one side of the pipe (i.e. Client->Server, Server->Client)
 func (input Data) Drop() bool {
 	return false
 }
 
-//Returns the string representation of the data. This string will be value logged to output.
+//PrettyPrint returns the string representation of the data. This string will be value logged to output.
 func (input Data) PrettyPrint() string {
 	return hex.Dump(input.Bytes)
 }
 
-//If this returns true, the PrettyPrinted version of the Data struct will be logged to output.
+//DoPrint will return true, if the PrettyPrinted version of the Data struct needs to be logged to the output.
 func (input Data) DoPrint() bool {
 	return true
 }
 
-//Returns true if data should be sent to the Trudy interceptor.
+//DoIntercept returns true if data should be sent to the Trudy interceptor.
 func (input Data) DoIntercept() bool {
 	return false
 }
