2e8cb314c2f1be41b53f251c1a7c5ea86533397a
Kelby Ludwig <kelbyludwig@gmail.com>
2015-11-30 17:44:43 -0600

    Swapped server client print output

--- main.go
+++ main.go
@@ -191,7 +191,7 @@ func serverHandler(pipe pipe.TrudyPipe) {
 		}
 
 		if data.DoPrint() {
-			log.Printf("Server -> Client: %v -> %v\n%v\n", data.SrcAddr.String(), data.DestAddr.String(), data.PrettyPrint())
+			log.Printf("Server -> Client: %v -> %v\n%v\n", data.DestAddr.String(), data.SrcAddr.String(), data.PrettyPrint())
 		}
 		_, err = pipe.WriteSource(data.Bytes[:bytesRead])
 		if err != nil {
